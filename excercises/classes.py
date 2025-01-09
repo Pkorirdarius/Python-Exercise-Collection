f4639ac826775e8b3a6d454ed4ead602d712fcf0
Pkorirdarius <83500619+Pkorirdarius@users.noreply.github.com>
2025-01-09 17:14:42 +0300

    Update classes.py

--- excercises/classes.py
+++ excercises/classes.py
@@ -1,10 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
 
 # # classes
-# 
-
-# In[7]:
 
 
 # defining a class
@@ -20,33 +15,17 @@ class Cars:
     def car_info(self):
         print(f"car name {self.name} ,car manufacturer {self.manufacturer} and the model is {self.model}")
 
-
-# In[8]:
-
-
-my_car = Cars('BMW','Volkswagen','M5')
-
-
-# In[10]:
-
-
-my_car.car_info()
-
-
-# In[6]:
-
-
 def car_build(name,manufacturer,model):
         car_make = {
-            "name":name,
-            "maufacturer": manufacturer,
-            "model":model
+            "name": name,
+            "manufacturer": manufacturer,
+            "model": model
         }
         return car_make
+# calling the classes
 
-
-# In[ ]:
-
+my_car = Cars('BMW','Volkswagen','M5')
+my_car.car_info()
 
 
 
